13721fc142be806abb6d46712f9a81856162725d
Pulsior <pulsior@gmail.com>
2014-04-09 22:35:36 +0200

    Some refactoring and an mcmod.info

--- java/com/pulsior/onepower/TheOnePower.java
+++ java/com/pulsior/onepower/TheOnePower.java
@@ -30,7 +30,7 @@ public class TheOnePower
     public static TheOnePower instance;
 	
     public static final String MODID = "theonepower";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.01";
     public static final String HUMAN_NAME = "The One Power";
     
     public static Item callandor;
